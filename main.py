46dc93b93b2d78a39112670e20e690af15034113
Josiah_f17 <josiah.foster@g.austincc.edu>
2024-10-19 00:36:00 +0000

    added a space to seperate numbers from box wall (left side)

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def startup_menu():
     print("RETURN/EXCHANGE ITEMS MENU:")
     print("--------------------------")
-    print("|1) Return Item          |")
-    print("|2) Exchange Item        |")
-    print("|3) Get Help             |")
-    print("|4) Exit                 |")
+    print("| 1) Return Item         |")
+    print("| 2) Exchange Item       |")
+    print("| 3) Get Help            |")
+    print("| 4) Exit                |")
     print("--------------------------")
 
 startup_menu()
