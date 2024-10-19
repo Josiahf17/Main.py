21d6df6c102685b0c6ccbdf59ee44d552131e153
Josiah_f17 <josiah.foster@g.austincc.edu>
2024-10-19 00:34:54 +0000

    created box and fixed options for startup menu function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def startup_menu():
     print("RETURN/EXCHANGE ITEMS MENU:")
     print("--------------------------")
     print("|1) Return Item          |")
-    print("|2) Excnahge Item        |")
+    print("|2) Exchange Item        |")
     print("|3) Get Help             |")
     print("|4) Exit                 |")
     print("--------------------------")
