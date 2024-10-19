31d96847842996c1cb1afd37669b5c1e872d2fed
Josiah_f17 <josiah.foster@g.austincc.edu>
2024-10-19 00:14:20 +0000

    Merge branch 'main' of https://github.com/Josiahf17/Main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def startup_menu():
     print("RETURN/EXCHANGE ITEMS MENU:")
-    print("--------------------------")
+    print("__________________________")
     print("Return Item")
     print("Excnahge Item")
     print("Get Help")
     print("Exit")
 
-    startup_menu()
+startup_menu()
