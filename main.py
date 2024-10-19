fa3e8d055b6d4288026ffeb1a535c2dcbde47ab4
Josiah_f17 <josiah.foster@g.austincc.edu>
2024-10-19 00:26:16 +0000

    removed space in between title and options in startup_menu function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def startup_menu():
     print("RETURN/EXCHANGE ITEMS MENU:")
     print("--------------------------")
-    print("\nReturn Item")
+    print("Return Item")
     print("Excnahge Item")
     print("Get Help")
     print("Exit")
