e3add3f1747a49a192e58369e124689746ea0371
Josiah_f17 <josiah.foster@g.austincc.edu>
2024-10-19 00:10:54 +0000

    Committed new main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def startup_menu():
+    print("RETURN/EXCHANGE ITEMS MENU:")
+    print("--------------------------")
+    print("Return Item")
+    print("Excnahge Item")
+    print("Get Help")
+    print("Exit")
+
+    startup_menu()
